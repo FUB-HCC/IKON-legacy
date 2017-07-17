08c8b65f19b6f43019cc80d7c840e44448b16453
Jonas <jonaso@web.de>
2017-07-17 11:28:31 +0200

    fix sys.argv error; add virtualenv

--- scraper/staff-scraper.py
+++ scraper/staff-scraper.py
@@ -26,13 +26,13 @@ workerList = htmlTree.find("div" , class_="panel-pane pane-views-panes pane-mita
 workerSet = {(link.get("href")[3:] if link.get("href").startswith("/de") else link.get("href")) for link in workerList}
 stafflist = []
 for link in workerSet:
-    workerHtmlTree = BeautifulSoup(requests.get(base_url + link).text, "lxml") 
+    workerHtmlTree = BeautifulSoup(requests.get(base_url + link).text, "lxml")
     staff = Staff(*Staff.populateObjectFromHTML(workerHtmlTree), base_url+link)
     print(staff.toJSON().decode('utf8'))
     stafflist.append(staff)
 
 # determine the output format and print in the corresponding format to a file
-if sys.argv[1   ] == "-csv":
+if len(sys.argv) > 1 and sys.argv[1   ] == "-csv":
     print("CSV")
     with open("staff.csv", "w+", newline = "\n") as csvout:
         writer = csv.writer(csvout, delimiter = ';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
@@ -53,4 +53,4 @@ with open('staff.json' , 'wb+') as jsonout:
         outstring.extend(staff.toJSON())
     outstring .extend("]".encode('utf8'))
     jsonout.write(outstring)
-    
+

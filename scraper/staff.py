4c7413cc1487a423242af83c16b3be92baf4c383
wittenator <wittenator@yahoo.de>
2017-09-13 12:11:17 +0200

    Fixed parsing of additional information and added a separation between "Forschung" and "Forschungsprojekte"

--- scraper/staff.py
+++ scraper/staff.py
@@ -5,10 +5,15 @@ from nameparser import HumanName
 from nameparser.config import Constants
 from bs4 import BeautifulSoup
 import requests
+from itertools import groupby
 
-def parsePublications(subtree, name):
+def parseInformation(subtree, name, mode):
+    # just get the text
+    if mode == 'text':
+        return subtree.find('div', class_=name).get_text()
+    # parse text by line breaks and <p> and <li> elements
     for p in subtree.find('div', class_=name).find_all(['p', 'li']):
-        for br in p.find_all("br"):
+        for br in p.find_all('br'):
             br.replace_with("/////")
     tempList = [p.text.split("/////") for p in subtree.find('div', class_=name).find_all(['p', 'li'])]
     #flatten list and remove elements without any text and inline css classes, which are sometimes scraped and start with \n
@@ -20,13 +25,15 @@ def normalizeTitel(titel):
             return "Publikationen"
         elif titel in ["Aufgabengebiet\n", "Aufgaben", "Tasks", "Aufgabengebiete"]:
             return "Aufgaben"
-        elif titel in ["Forschungsprojekte", "Research", "Projekte", "Forschung", "Forschungsinteressen", "Forschung", "Forschungsschwerpunkt"]:
+        elif titel in ["Research", "Forschung", "Forschungsinteressen", "Forschung", "Forschungsschwerpunkt"]:
             return "Forschung"
+        elif titel in ["Forschungsprojekte", "Projekte"]:
+            return "Forschungsprojekte"
         else:
             return titel
 
 class Staff(object):
-    def __init__(self, name, email, telefon, fax, address, photo, skills, cv, url):
+    def __init__(self, name, email, telefon, fax, address, photo, skills, url):
         self.name = name
         self.email = email
         self.telefon = telefon
@@ -35,7 +42,6 @@ class Staff(object):
         self.photo = photo
         self.skills = skills
         self.url = url
-        self.cv = cv
 
     def toJSON(self):
         return json.dumps(self.__dict__, indent=4, sort_keys=True, ensure_ascii=False).encode('utf8')
@@ -44,62 +50,79 @@ class Staff(object):
         return 
 
     def populateObjectFromHTML(tree):
-        list = []
+        proplist = []
         base_url = "https://www.naturkundemuseum.berlin"
         # scrapes the contact info section
         arguments = ['Name','Email', 'Telefon', 'Fax', 'Adresse']
         for info in arguments:
             try:
-                list.append(tree.find('div', class_=("views-field views-field-"+info)).find('span', class_="field-content").get_text().replace("\r\n", ","))
+                proplist.append(tree.find('div', class_=("views-field views-field-"+info)).find('span', class_="field-content").get_text().replace("\r\n", ","))
             except AttributeError:
-                list.append(None)
+                proplist.append(None)
 
         #scrapes the photo URI
-        list.append(base_url + tree.find('div', class_="views-field views-field-img-URL").span.img.get('src'))
+        proplist.append(base_url + tree.find('div', class_="views-field views-field-img-URL").span.img.get('src'))
 
         #scrapes the accordion
         accordion = {}
         # get all accordion entries
         for element in tree.find_all('div', class_="ui styled accordion"):
             titel = normalizeTitel(re.sub(r"[^\w .()]", "", element.find('div', class_="ac_title_text").get_text()).strip())
-            # get the content of a list in a accordion
-            if "ul" in [elem.name for elem in element.find('div', class_="content").descendants]:
-                #print("Liste gefunden")
-                accordion[titel] = [re.sub(r"[^\w .()]", "",li.text.strip()) for li in element.find('div', class_="content").find_all('li')]
             # get all publications and parse them by <br/>'s
-            elif titel == "Publikationen" :
-                accordion[titel] = parsePublications(element, "content")
-                #try to get additional publications
-                try:
-                    for link in tree.find('div', class_="view-display-id-single_person_sidebar_view").findAll('a', href=True, text="Publikationen"):
-                        pubTree = BeautifulSoup(requests.get(base_url+link.get('href')).text, 'lxml')
-                        accordion[titel] += parsePublications(pubTree, "faqfield-answer")
-                except AttributeError:
-                    pass
+            if titel == "Publikationen":
+                accordion[titel] = parseInformation(element, "content", 'list')
+            # search in the "Forschung" entry for an "Forschungsprojekte" entry to extract it
+            elif titel == "Forschung":
+                templist = [re.sub(r"[^\w .()-:/]", "",li.text.strip()) for li in element.find('div', class_="content").find_all(['li', 'p'])]
+                secondtemplist = [list(group) for k, group in groupby(templist, lambda x: re.match(r"((.{0,15}Forschungsprojekt.*)|(.{0,15}Projekt.*))", x)) if not k]
+                if len(secondtemplist) > 1:
+                    if 'Forschungsprojekte' in accordion:
+                        accordion['Forschungsprojekte'] += secondtemplist.pop(1)
+                    else:
+                        accordion['Forschungsprojekte'] = secondtemplist.pop(1)
+                if len(secondtemplist) > 0:
+                    accordion[titel] = [element for sublist in secondtemplist for element in sublist if element is not ""]
             # if nothing matches just get the text of the element
             else:
-                accordion[titel] = [re.sub(r"[^\w .()]", "", element.find('div', class_="content").get_text().strip())]
-        list.append(accordion)
+                if titel in accordion:
+                    accordion[titel] += [re.sub(r"[^\w .()-:/]", "",li.text.strip()) for li in element.find('div', class_="content").find_all(['li', 'p'])]
+                else:
+                    accordion[titel] = [re.sub(r"[^\w .()-:/]", "",li.text.strip()) for li in element.find('div', class_="content").find_all(['li', 'p'])]
+        proplist.append(accordion)
 
-        #try to find a CV
+        #try to find additional informations
         try:
-            cvlist = tree.find('div', class_="view-display-id-single_person_sidebar_view").findAll('a', href=True, text="Lebenslauf")
-            if not cvlist:
-                list.append(None)
-            for link in cvlist:
-                print("Gefunden")
-                cvTree = BeautifulSoup(requests.get(base_url+link.get('href')).text, 'lxml')
-                list.append(parsePublications(cvTree, "faqfield-answer"))
+            for link in tree.find('div', class_="view-display-id-single_person_sidebar_view").findAll('a', href=True):
+                print(link.text)
+                titel = normalizeTitel(link.text.strip())
+                if titel == 'Lebenslauf':
+                    print("CV Gefunden")
+                    infoTree = BeautifulSoup(requests.get(base_url+link.get('href')).text, 'lxml')
+                    proplist[6]['CV'] = parseInformation(infoTree, "faqfield-answer", 'text')
+                elif titel == 'Publikationen':
+                    print("Publikation Gefunden")
+                    infoTree = BeautifulSoup(requests.get(base_url+link.get('href')).text, 'lxml')
+                    if titel in proplist[6]:
+                        proplist[6][titel] += parseInformation(infoTree, "faqfield-answer", 'list')
+                    else:
+                        proplist[6][titel] = parseInformation(infoTree, "faqfield-answer", 'list')
+                else:
+                    print("Etwas anderes Gefunden")
+                    infoTree = BeautifulSoup(requests.get(base_url+link.get('href')).text, 'lxml')
+                    if titel in proplist[6]:
+                        proplist[6][titel] += parseInformation(infoTree, "faqfield-answer", 'text')
+                    else:
+                        proplist[6][titel] = parseInformation(infoTree, "faqfield-answer", 'text')
         except AttributeError:
-            list.append(None)
+            print('No additional information was found')
 
 
         # set up the name parser
         constants = Constants()
         constants.titles.add('PD', 'Dipl.', 'des.', 'Professor', 'M.Sc.', 'FH')
         # parse the name and normalize weird writing styles for "Ph.D."
-        list[0] = HumanName(re.sub(r"Ph\. D\.", "Ph.D." ,list[0]), constants=constants).as_dict()
-        return list
+        proplist[0] = HumanName(re.sub(r"Ph\. D\.", "Ph.D." ,proplist[0]), constants=constants).as_dict()
+        return proplist
 
 
 

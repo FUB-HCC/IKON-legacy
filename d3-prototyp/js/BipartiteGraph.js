7b7801a71d51b6e42d95a27f1dc2b456d8d2bc68
jonaskopka <jonas.kopka@gmail.com>
2017-11-16 18:21:12 +0100

    Adding hrefGlobal

--- d3-prototyp/js/BipartiteGraph.js
+++ d3-prototyp/js/BipartiteGraph.js
@@ -6,22 +6,22 @@ data1 = [{
 		{
 			source: 	"forschungsbereich",
 			target: 	"hauptthema",
-			value: 		10,
+			value: 		7,
 		},
 		{
 			source: 	"geldgeber",
 			target: 	"kooperationspartner",
-			value: 		10,
+			value: 		7,
 		},
 		{
 			source: 	"nebenthemen",
 			target: 	"nebenthemen",
-			value: 		10,
+			value: 		7,
 		},
 		{
 			source: 	"start",
 			target: 	"start",
-			value: 		10,
+			value: 		15,
 		}]
 
 function createBipartiteGraph(p1,p2){
@@ -60,28 +60,56 @@ function createBipartiteGraph(p1,p2){
 
 	var path = sankey.link();
 
-	var div = d3.select("body").append("div")
-	    .attr("class", "tooltipsankey")
-	    .style("opacity", 0);
+	/*var div = d3.select("body").append("div")
+	    .attr("class", "tooltip")
+	    .style("opacity", 0);*/
 
 	var color = d3.scaleOrdinal()
 	    .domain(["forschungsbereich",".forschungsbereich","hauptthema",".hauptthema","nebenthemen",".nebenthemen","start",".start","geldgeber",".geldgeber","kooperationspartner",".kooperationspartner"])
-	    .range(["yellowgreen","yellowgreen", "darkblue", "darkblue", "orange","orange", "gold","gold","lawngreen","lawngreen", "firebrick","firebrick"]);
-	    // , "grey",
+	    .range(["#985152","#985152", "#7d913c", "#7d913c", "#8184a7","#8184a7", "#d9ef36","#d9ef36","#985152","#985152", "#7d913c","#7d913c"]);
+	    // , "grey","#985152","#7d913c","#8184a7","#d9ef36"
 
 	var rect;
 	var node;
 	var link;
 
-	graph = {"nodes" : [], "links" : []};
-    data1.forEach(function (d) {
-    	graph.nodes.push({ "name": d.source});
-		graph.nodes.push({ "name": "."+d.target});
+	var nodeNames = ["forschungsbereich",".forschungsbereich","hauptthema",".hauptthema",
+			"nebenthemen",".nebenthemen","start",".start","end",".end","geldgeber",".geldgeber",
+			"kooperationspartner",".kooperationspartner","antragsteller",".antragsteller",
+			"projektleiter",".projektleiter","titel",".titel"];
+
+	graph = {"nodes" : [{"name":"junk"},{"name":".junk"}],
+			 "links" : []};
+
+	data1.forEach(function (d) {
 		graph.links.push({  "source": d.source,
 							"target": "."+d.target,
-							"color": d.color,
+							"color": "#faf0fa",
                         	"value": +d.value });
     });
+
+
+
+	nodeNames.forEach(function (d) {
+    	graph.nodes.push({"name":d})
+    	//if( !graph.links.some(item => item.source === d.substring(1))
+    	//	&& !graph.links.some(item => item.target === d)){
+	    	if(d.substring(0,1)=="."){
+	    		graph.links.push({  "source": "junk",
+								"target": d,
+								"color": "none",
+	                        	"value": 5});
+	    	}else{
+	    		graph.links.push({  "source": d,
+								"target": ".junk",
+								"color": "none",
+	                        	"value": 5});
+	    	}
+    	//}
+
+	});
+
+
     graph.nodes = d3.keys(d3.nest()
     		.key(function (d) { return d.name; })
     		.object(graph.nodes));
@@ -93,10 +121,11 @@ function createBipartiteGraph(p1,p2){
     	if(d.substring(0,1)==="."){
     		graph.nodes[i] = { "name": d, "text": p1[d.substring(1)]};
     	}else{
-    		graph.nodes[i] = { "name": d, "text": p1[d]};
+    		graph.nodes[i] = { "name": d, "text": p2[d]};
     	}
 
     });
+    console.log(graph);
   	sankey.nodes(graph.nodes)
     		.links(graph.links)
     		.layout(32);
@@ -108,14 +137,14 @@ function createBipartiteGraph(p1,p2){
 		      .attr("d", path)
 			  .attr("id", function(d) { return "link" + d.source.name; })
 		      .style("stroke-width", function(d) { return Math.max(1, d.dy); })
-			  .style("stroke", function(d) { /*return d.color;*/return "#faf0fa"; });
+			  .style("stroke", function(d) { return d.color; });
 	link.on("mouseover", function(d) {
-            div.transition()
+            /*div.transition()
                 .duration(200)
                 .style("opacity", .9);
             div .html("<b>" + d.source.name + "</b> -> <b>"  + d.target.name + "</b><br/>" + format(d.value))
                 .style("left", (d3.event.pageX) + "px")
-                .style("top", (d3.event.pageY - 28) + "px");
+                .style("top", (d3.event.pageY - 32) + "px");*/
 
 			link.transition()
 		        .duration(700)
@@ -126,9 +155,9 @@ function createBipartiteGraph(p1,p2){
 				.style("opacity", 1);
 	})
         .on("mouseout", function(d) {
-            div.transition()
+            /*div.transition()
                 .duration(500)
-                .style("opacity", 0);
+                .style("opacity", 0);*/
 
             gAll.selectAll(".linksankey").transition()
         		.duration(700)
@@ -140,27 +169,37 @@ function createBipartiteGraph(p1,p2){
 	    .enter().append("g")
 	      .attr("class", "nodesankey")
 	      .attr("transform", function(d) {
+	      		/*if(d.name.substring(0,1) !== "."){
+	      			d.x=0;
+	      		}*/
 			  return "translate(" + d.x + "," + d.y + ")"; });
 
 	rect = node.append("rect")
 	      .attr("height", function(d) {
-	      	 return d.dy; })
+	      	if(d.name=="junk"||d.name==".junk"){
+	      		return 0;
+	      	}
+	      	/*if(d.dy == 0){
+	      		return 10;
+	      	}*/
+	      	return d.dy;
+	      })
 	      .attr("width", sankey.nodeWidth())
 	      .style("fill", function(d) {
 	      	return color(d.name); });
 
 	rect.on("mouseover", function(d) {
-	            div.transition()
+	            /*div.transition()
 	                .duration(200)
 	                .style("opacity", .9);
 	            div .html("<b>" + d.name + "</b>:<br/>" + format(d.value))
 	                .style("left", (d3.event.pageX) + "px")
-	                .style("top", (d3.event.pageY - 28) + "px");
+	                .style("top", (d3.event.pageY - 28) + "px");*/
 	            })
 	        .on("mouseout", function(d) {
-	            div.transition()
+	            /*div.transition()
 	                .duration(500)
-	                .style("opacity", 0);
+	                .style("opacity", 0);*/
 	        });
 
 	  node.append("text")
@@ -169,7 +208,19 @@ function createBipartiteGraph(p1,p2){
 	      .attr("dy", ".35em")
 	      .attr("text-anchor", "start")
 	      .attr("transform", null)
+	      .style("fill",function(d) {
+	      	return color(d.name);
+	      })
 	      .text(function(d) {
+	      	if(d.name=="junk"||d.name==".junk"){
+	      		return "";
+	      	}
+	      	if(d.name == "forschungsbereich"||d.name == ".forschungsbereich"){
+	      		return "Forschungsbereich "+ d.text;
+	      	}else if (d.name == "start"||d.name == ".start"||d.name == "end"||d.name == ".end"){
+	      		var dt = d.text;
+	      		return  dt.getDate() + "." + (dt.getMonth() + 1) + "." + dt.getFullYear();
+	      	}
 	      	return d.text; })
 		  .attr("class", "graph")
 	    .filter(function(d) { return d.x < width / 2; })

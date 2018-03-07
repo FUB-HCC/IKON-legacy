99136b94efc29ba04d4d6574587dc4f9fa51bcf3
wittenator <wittenator@yahoo.de>
2018-03-07 16:06:25 +0100

    Added NLPverifier server

---
+++ IKON-NLPverifier/server.js
@@ -0,0 +1,113 @@
+const express = require('express')
+const multer  = require('multer')
+const fs = require('fs-extra')
+const util = require('util')
+const app = express()
+
+// set up frontend files
+const frontend = fs.readFileSync('./frontend/build/index.html')
+app.use(express.static(__dirname + "/frontend/build"));
+
+// define important constants
+const PORT = process.env.PORT || 8080
+const SAVEDIR = process.env.SAVEDIR || './files/projects/'
+const WORKDIR = process.env.WORKDIR || './files/annotated_projects/'
+
+const storage = multer.diskStorage({
+  destination: SAVEDIR,
+  filename: function (req, file, cb) {
+    cb(null, file.originalname + '-' + Date.now())
+  }
+})
+
+const validFileParam = async (file) => {
+    return new Promise((resolve, reject) => {
+        fs.readdir(SAVEDIR)
+            .then(files => {
+                console.log(files, file, files.includes(file))
+                resolve(files.includes(file))
+            })
+    })
+}
+
+const acceptedFileTypes = (file) => {
+    return file.mimetype === 'application/json'
+}
+
+const fileFilter = (req, file, cb) => {
+    cb(null, acceptedFileTypes(file))  
+}
+
+const setLeftSetDifference = (set1, set2) => {
+    return set1.filter(x => set2.indexOf(x) < 0 )
+}
+
+const upload = multer({
+    fileFilter: fileFilter,
+    storage: storage
+})
+
+// get frontend
+app.get('/', (req, res, next) => {
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.end(frontend)
+})
+
+// get all files with name and id
+app.get('/files', (req, res) => {
+    fs.readdir(SAVEDIR)
+    .then(files => res.json(files.map(file => {return {name: file.substring(0, file.length-14), fileID: file}}))
+                        .status(200)
+                        .end())
+
+})
+
+//saves a file in order to annotate it later
+app.put('/files', upload.single('data'), (req, res) => {
+    res.status((req.file)?200:422).end()
+})
+
+// gets an entry from the file :file
+app.get('/files/:file', async (req, res) => {
+    if(await validFileParam(req.params.file)){
+        res.json(JSON.parse(fs.readFileSync(SAVEDIR + req.params.file)).pop())
+        .status(200)
+        .end()
+    }
+    else{
+        res.status(404).end()
+    }
+})
+
+// appends an annotation to the annotated file
+app.post('/files/:file', async (req, res) => {
+    if(await validFileParam(req.params.file)){
+        fs.pathExists(WORKDIR + req.params.file)
+            .then(exists => {
+                if (!exists) {
+
+                }
+            })
+            .then()
+            .then()
+    }
+    else{
+        res.status(404).end()
+    }
+})
+
+// resets an annotation by deleting the annotated file
+app.delete('/files/:file', async (req, res) => {
+    if (await validFileParam(req.params.file)) {
+        fs.unlink(WORKDIR + req.params.file, (err) => {
+            res.send((err)?err:'File deleted!').status((err)?500:200).end()
+        })
+    }
+    else{
+        res.status(404).end()
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}!`)
+})
